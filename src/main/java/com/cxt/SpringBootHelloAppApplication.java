941007fc9cc2c7482947f89d97ad307f064499d8
cxtdev <43331273+cxtdev@users.noreply.github.com>
2018-09-25 12:00:47 +0530

    Update SpringBootHelloAppApplication.java

--- src/main/java/com/cxt/SpringBootHelloAppApplication.java
+++ src/main/java/com/cxt/SpringBootHelloAppApplication.java
@@ -16,7 +16,7 @@ public class SpringBootHelloAppApplication {
 	@RequestMapping("/hello")
 	public String hello() {
 
-		return "\"result \": {\n" + "\n" + "\"greetings\" : \"Test Page \" \n" + "\n" + "}";
+		return "\"result \": {\n" + "\n" + "\"greetings\" : \"This is a Test Page generated using Code Pipeline and deployed to ECS\" \n" + "\n" + "}";
 
 	}
 }
